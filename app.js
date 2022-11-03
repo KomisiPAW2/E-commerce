ff9d7ec1f34c3c784f9943a676c1ac2c5d6c99eb
Jalalul Mu'ti <Jalalul2000@gmail.com>
2022-11-03 08:41:30 +0700

    Update App.js Admin

--- app.js
+++ app.js
@@ -37,6 +37,81 @@ app.get('/pembelian', (req, res) => {
     res.render('pembelian.ejs');
 });
 
+// Admin
+app.get('/admin', (req, res) => {
+    res.render('admin/login-admin.ejs');
+});
+
+app.get('/dashboard', (req,res) => {
+    connection.query('select * from tb_toko', (err, results) => {
+        if (err) {
+            throw error;
+        }
+        res.render('admin/index-admin.ejs', {items:results});
+    });
+});
+
+app.get('/edit-profil-toko', (req,res) => {
+    res.render('admin/editProfil.ejs');
+});
+
+app.post('/edit', (req,res) => {
+    var nama = req.body.nama_toko;
+    var alamat = req.body.alamat_toko;
+    var deskripsi = req.body.deskripsi_toko;
+    var nomor = req.body.nomor_telepon;
+    var email = req.body.email;
+
+    connection.query ('UPDATE tb_toko SET nama_toko=?, alamat_toko=?, deskripsi_toko=?, nomor_telepon=?, email=? WHERE id_toko=1', [nama],[alamat],[deskripsi],[nomor],[email], (error, results) => {
+        if (error) {
+            throw error;
+        }
+
+        // isi method get (index)
+        res.redirect('/dashboard');
+    });
+});
+
+app.get('/asset', (req,res) => {
+    res.render('admin/assets.ejs')
+});
+
+app.get('/databarang', (req,res) => {
+    res.render('admin/dataBarang.ejs')
+});
+
+app.get('/kategori', (req,res) => {
+    res.render('admin/kategori.ejs')
+});
+
+app.get('/approve', (req,res) => {
+    res.render('admin/approve.ejs')
+});
+
+app.get('/laporanPenjualan', (req,res) => {
+    res.render('admin/lapPenjualan.ejs')
+});
+
+app.get('/tambahAset', (req,res) => {
+    res.render('admin/tambahAset.ejs')
+});
+
+app.get('/tambahDatabarang', (req,res) => {
+    res.render('admin/tambahData.ejs')
+});
+
+app.get('/tambahKategori', (req,res) => {
+    res.render('admin/tambahKategori.ejs')
+});
+
+app.get('/tambahJenis', (req,res) => {
+    res.render('admin/tambahJenis.ejs')
+});
+
+app.get('/detailLaporan', (req,res) => {
+    res.render('admin/detailLaporan.ejs')
+});
+
 //Database
 var mysql=require('mysql');
  var connection=mysql.createConnection({
@@ -52,3 +127,5 @@ connection.connect(function(err){
      console.log('Connected!:)');
    }
  });  
+
+ app.use(express.urlencoded({extended:false}));
