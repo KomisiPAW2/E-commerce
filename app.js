2fbe3b2fceff6af2de7f64991d697776cca44659
Muhamad Iqbal Setiawan <101556141+muhamadiqbalsetiawan@users.noreply.github.com>
2022-11-03 08:28:23 +0700

    Commit app.js  laman Admin

--- app.js
+++ app.js
@@ -1,54 +1,62 @@
 const express = require('express');
 const app = express();
-// Menentukan server port
+
+app.set('view engine', 'ejs')
+
 app.listen(3000, ()=> {
     console.log('server running on port 3000');
 });
-// Agar server mengenali folder public
+
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-        res.render('login.ejs');
-});
-app.get('/signup', (req, res) => {
-    res.render('signup.ejs');
-});
-app.get('/forgotPassword', (req, res) => {
-    res.render('forgotPassword.ejs');
-});
-app.post('/submit', (req, res) => {
-    res.render('index.ejs');
-});
-app.post('/', (req, res) => {
-    res.render('login.ejs');
-});
-app.get('/cart', (req, res) => {
-    res.render('cart.ejs');
-});
-app.get('/about', (req, res) => {
-    res.render('about.ejs');
-});
-app.get('/detail', (req, res) => {
-    res.render('detail.ejs');
-});
-app.get('/index', (req, res) => {
-    res.render('index.ejs');
-});
-app.get('/pembelian', (req, res) => {
-    res.render('pembelian.ejs');
+    res.render('index.ejs')
 });
 
-//Database
-var mysql=require('mysql');
- var connection=mysql.createConnection({
-   host:'localhost',
-   user:'root',
-   password:'',
-   database:'waroeng'
- });
-connection.connect(function(err){
-   if(err){
-     console.log(error);
-   }else{
-     console.log('Connected!:)');
-   }
- });  
+app.get('/index', (req, res) => {
+    res.render('index.ejs')
+});
+
+app.get('/assets', (req, res) => {
+    res.render('assets.ejs')
+});
+
+app.get('/dataBarang', (req, res) => {
+    res.render('dataBarang.ejs')
+});
+
+app.get('/kategori', (req, res) => {
+    res.render('kategori.ejs')
+});
+
+app.get('/approve', (req, res) => {
+    res.render('approve.ejs')
+});
+
+app.get('/lapPenjualan', (req, res) => {
+    res.render('lapPenjualan.ejs')
+});
+
+app.get('/editProfil', (req, res) => {
+    res.render('editProfil.ejs')
+});
+
+app.get('/tambahAset', (req, res) => {
+    res.render('tambahAset.ejs')
+});
+
+app.get('/tambahData', (req, res) => {
+    res.render('tambahData.ejs')
+});
+
+app.get('/tambahKategori', (req, res) => {
+    res.render('tambahKategori.ejs')
+});
+
+app.get('/tambahJenis', (req, res) => {
+    res.render('tambahJenis.ejs')
+});
+
+app.get('/detailLaporan', (req, res) => {
+    res.render('detailLaporan.ejs')
+});
